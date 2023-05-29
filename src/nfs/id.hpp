7132af20c13d8fb622cda8d46a73ec3212c62d7b
cydiater <cydiater@gmail.com>
2023-05-29 18:01:47 +0000

    files

---
+++ src/nfs/id.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <atomic>
+
+#include "nfs/config.hpp"
+
+class IDManager {
+  std::atomic<uint32_t> cur_idx;
+
+public:
+  static const uint32_t root_inode_idx = 0;
+
+  IDManager() { cur_idx = root_inode_idx; }
+
+  int allocate() { return ++cur_idx; }
+};
---
+++ src/nfs/utils.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+std::pair<std::vector<std::string>, std::string>
+parse_path_components(const char *path) {
+  // todo
+}
