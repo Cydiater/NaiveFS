2063e71d58752e53668f0fa6d83bbb72ce1be53f
cYd1αtεr <cydiater@gmail.com>
2023-05-29 22:44:18 +0800

    补充基本的框架 (#7)

---
+++ src/nfs/config.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+constexpr uint32_t kInodeDirectCnt = 24;
+constexpr char *kDiskPath = "/tmp/disk";
+constexpr uint32_t kDiskCapacityGB = 16;
+constexpr uint32_t kBlockSize = 4 * 1024;
+constexpr uint32_t kMaxInode = 4096;
+constexpr uint32_t kCRSize = kMaxInode * 4;
---
+++ src/nfs/inode.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "nfs/config.hpp"
+
+class Inode {
+  uint32_t size, access_time, modify_time, change_time;
+  uint16_t uid, gid, link_cnt, mode;
+  uint32_t directs[kInodeDirectCnt];
+  uint32_t indirect1, indirect2;
+
+public:
+  void read(char *buf, uint32_t offset, uint32_t size) {
+    // todo
+  }
+};
